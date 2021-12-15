1289d394d0063c6686b73820ab088c3129738780
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-12-14 20:28:30 -0600

    Start of theme usage

--- Stardrop/App.axaml.cs
+++ Stardrop/App.axaml.cs
@@ -1,9 +1,14 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+using Stardrop.Utilities;
 using Stardrop.ViewModels;
 using Stardrop.Views;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Stardrop
 {
@@ -12,6 +17,24 @@ namespace Stardrop
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
+
+            // Handle adding the themes
+            Dictionary<string, IStyle> themes = new Dictionary<string, IStyle>();
+            foreach (string fileFullName in Directory.EnumerateFiles("Themes", "*.xaml"))
+            {
+                try
+                {
+                    var themeName = Path.GetFileNameWithoutExtension(fileFullName);
+                    themes[themeName] = AvaloniaRuntimeXamlLoader.Parse<Styles>(File.ReadAllText(fileFullName));
+                    Program.helper.Log($"Loaded theme {Path.GetFileNameWithoutExtension(fileFullName)}", Helper.Status.Debug);
+                }
+                catch (Exception ex)
+                {
+                    Program.helper.Log($"Unable to load theme on {Path.GetFileNameWithoutExtension(fileFullName)}: {ex}", Helper.Status.Warning);
+                }
+            }
+
+            Styles.Insert(0, themes.Values.First());
         }
 
         public override void OnFrameworkInitializationCompleted()
