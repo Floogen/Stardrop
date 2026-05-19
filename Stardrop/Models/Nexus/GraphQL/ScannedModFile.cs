3489a3c378154f376d5c4fe5a4ca92adfb099871
Floogen <31755155+Floogen@users.noreply.github.com>
2026-05-18 19:51:25 -0500

    Prevented quarantined mod updates from being downloaded

---
+++ Stardrop/Models/Nexus/GraphQL/ScannedModFile.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Stardrop.Models.Nexus.GraphQL
+{
+    public class ScannedModFile
+    {
+        [JsonConverter(typeof(JsonStringEnumConverter<VirusScanStatus>))]
+        public enum VirusScanStatus
+        {
+            NOT_SCANNED,
+            QUEUED,
+            WAITING_REPORT,
+            VERIFIED,
+            INTERNALLY_VERIFIED,
+            QUARANTINED,
+            MANUALLY_VERIFIED,
+            MOD_DOES_NOT_EXIST,
+            FILE_NOT_FOUND,
+            REPORT_ERROR,
+            TOO_LARGE
+        }
+
+        [JsonPropertyName("fileId")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("scannedV2")]
+        public VirusScanStatus VirusScanResults { get; set; }
+    }
+}
