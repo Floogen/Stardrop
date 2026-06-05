3f710803da19215d05dfbf15501d1a7a325ebcaf
Floogen <31755155+Floogen@users.noreply.github.com>
2026-06-04 23:04:38 -0500

    Added design mode only settings for FlexibleOptionWindow

--- Stardrop/ViewModels/FlexibleOptionWindowViewModel.cs
+++ Stardrop/ViewModels/FlexibleOptionWindowViewModel.cs
@@ -1,4 +1,7 @@
+using Avalonia.Controls;
 using ReactiveUI;
+using Stardrop.Models;
+using System;
 
 namespace Stardrop.ViewModels
 {
@@ -19,5 +22,21 @@ namespace Stardrop.ViewModels
         public bool IsSecondButtonVisible { get { return _isSecondButtonVisible; } set { this.RaiseAndSetIfChanged(ref _isSecondButtonVisible, value); } }
         private bool _isThirdButtonVisible;
         public bool IsThirdButtonVisible { get { return _isThirdButtonVisible; } set { this.RaiseAndSetIfChanged(ref _isThirdButtonVisible, value); } }
+
+        public FlexibleOptionWindowViewModel()
+        {
+            if (Design.IsDesignMode)
+            {
+                MessageText = "Example text";
+
+                FirstButtonText = "Yes";
+                SecondButtonText = "Maybe";
+                ThirdButtonText = "Cancel";
+
+                IsFirstButtonVisible = true;
+                IsSecondButtonVisible = true;
+                IsThirdButtonVisible = true;
+            }
+        }
     }
 }
